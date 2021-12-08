ad6528ab97317a81a6e37ac5cc0bffd5ddf05f40
mirgo <rimlog.94@gmail.com>
2021-12-08 22:08:49 +0300

    Initial commit

---
+++ src/main/java/Box/Task.java
@@ -0,0 +1,15 @@
+package Box;
+
+    class Task1 {
+        static void swapObj(Object[] array, int firstIndex, int secondIndex) {
+            Object oneVal = array[firstIndex];
+            array[firstIndex] = array[secondIndex];
+            array[secondIndex] = oneVal;
+        }
+
+        static <T> ArrayList<T> convertToList(T[] array) {
+            return new ArrayList<>(Arrays.asList(array));
+        }
+    }
+
+
